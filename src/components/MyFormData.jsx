3f779f9393d3898e88156e5583b8786d87a215e5
sim-sal <simone.saladino93@gmail.com>
2023-12-06 18:44:55 +0100

    add style and bg-img

--- src/components/MyFormData.jsx
+++ src/components/MyFormData.jsx
@@ -53,130 +53,136 @@ export default function MyFormData() {
     }
 
     return (
-        <div className={"container"}>
-            <h1 className="text-center">CREA POST</h1>
 
-            <form onSubmit={handleFormSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="titlePost" className="form-label">
-                        <FontAwesomeIcon icon="fa-solid fa-tornado" />
-                        <strong> Titolo:</strong>
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Inserisci qui il titolo del post"
-                        name="title_post"
-                        value={formData.title_post}
-                        onChange={(e) => updateFormData(e.target.value, "title_post")}
-                    ></input>
+        <div className={`container-fluid ${style.container_fluid_mod}`}>
+            <h1 className="text-center text-light"><strong>CREA POST</strong></h1>
 
-                    <label htmlFor="imgPost" className="form-label">
-                        <FontAwesomeIcon icon="fa-solid fa-image" />
-                        <strong> URL Immagine:</strong>
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Inserisci l'URL dell'immagine"
-                        name="img_post"
-                        value={formData.img_post}
-                        onChange={(e) => updateFormData(e.target.value, "img_post")}
-                    ></input>
+            <div className={style.my_form}>
+                <form onSubmit={handleFormSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="titlePost" className="form-label">
+                            <FontAwesomeIcon icon="fa-solid fa-tornado" />
+                            <strong> Titolo:</strong>
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Inserisci qui il titolo del post"
+                            name="title_post"
+                            value={formData.title_post}
+                            onChange={(e) => updateFormData(e.target.value, "title_post")}
+                        ></input>
 
-                    <label htmlFor="contentPost" className="form-label">
-                        <FontAwesomeIcon icon="fa-solid fa-box-open" />
-                        <strong> Contenuto:</strong>
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Inserisci qui il contenuto del post"
-                        name="content_post"
-                        value={formData.content_post}
-                        onChange={(e) => updateFormData(e.target.value, "content_post")}
-                    ></input>
-                </div>
+                        <label htmlFor="imgPost" className="form-label">
+                            <FontAwesomeIcon icon="fa-solid fa-image" />
+                            <strong> URL Immagine:</strong>
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Inserisci l'URL dell'immagine"
+                            name="img_post"
+                            value={formData.img_post}
+                            onChange={(e) => updateFormData(e.target.value, "img_post")}
+                        ></input>
 
-                <div>
-                    <label htmlFor="categoryPost" className="form-label">
-                        <FontAwesomeIcon icon="fa-solid fa-certificate" />
-                        <strong> Categoria:</strong>
-                    </label><br />
-                    <select value={formData.category_post} onChange={(e) => updateFormData(e.target.value, 'category_post')}>
-                        <option value="">--Seleziona la categoria--</option>
-                        <option value="Nature Notes" /*selected={formData.category_post === 'nature_notes'}*/>Nature Notes</option>
-                        <option value="Travel Tidbits" /*selected={formData.category_post === 'travel_tidbits'}*/>Travel Tidbits</option>
-                        <option value="Nature's Palette" /*selected={formData.category_post === 'natures_palette'}*/>Nature's Palette</option>
-                        <option value="Wayfaring Waves" /*selected={formData.category_post === 'wayfaring_waves'}*/>Wayfaring Waves</option>
-                        <option value="Nomad Notes" /*selected={formData.category_post === 'nomad_notes'}*/>Nomad Notes</option>
-                        <option value="Sound Bites" /*selected={formData.category_post === 'sound_bites'}*/>Sound Bites</option>
-                    </select>
-                </div>
-
-                <label htmlFor="tagsPost" className="form-label">
-                    <FontAwesomeIcon icon="fa-solid fa-hashtag" />
-                    <strong> Tags:</strong>
-                </label>
-                <div className="flex gap-4">
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('LifeStyle')} value="LifeStyle" onChange={handleTagsPostChange} />#LifeStyle</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('MotivationMonday')} value="MotivationMonday" onChange={handleTagsPostChange} />#MotivationMonday</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('PicOfTheDay')} value="PicOfTheDay" onChange={handleTagsPostChange} />#PicOfTheDay</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('NaturePhotography')} value="NaturePhotography" onChange={handleTagsPostChange} />#NaturePhotography</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('AdventureTime')} value="AdventureTime" onChange={handleTagsPostChange} />#AdventureTime</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('MusicIsLife')} value="MusicIsLife" onChange={handleTagsPostChange} />#MusicIsLife</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('MusicInspiration')} value="MusicInspiration" onChange={handleTagsPostChange} />#MusicInspiration</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('VacationMode')} value="VacationMode" onChange={handleTagsPostChange} />#VacationMode</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('TravelPhotography')} value="TravelPhotography" onChange={handleTagsPostChange} />#TravelPhotography</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('DigitalWorld')} value="DigitalWorld" onChange={handleTagsPostChange} />#DigitalWorld</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('FilmFestival')} value="FilmFestival" onChange={handleTagsPostChange} />#FilmFestival</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('SocialMedia')} value="SocialMedia" onChange={handleTagsPostChange} />#SocialMedia</label>
-                    <label htmlFor="" className="px-3"><input type="checkbox" checked={formData.tags_post.includes('TagsForLike')} value="TagsForLike" onChange={handleTagsPostChange} />#TagsForLike</label>
-                </div>
-
-                <div>
-                    <button type="submit" className="btn btn-primary">
-                        Crea Post
-                    </button>
-                </div>
-
-            </form>
-
-            <div className={`row justify-content-center py-5`}>
-                {postsList.map((post) => (
-                    <div key={post.id} className={`col-5 mx-5 my-5 card ${style.mod_card}`}>
-
-                        <img className={style.card_img} src={post.img_post} alt="Post" />
-
-                        <div className={style.card_main}>
-                            <div className={style.card_header}>
-
-                                <h3>{post.title_post}</h3>
-
-                                <button className={style.delete_button} onClick={() => removePost(post.id)}>
-                                    <FontAwesomeIcon icon="fa-solid fa-trash" />
-                                </button>
-
-                            </div>
-
-                            <p>{post.content_post}</p>
-
-                            <div>
-                                <h5>Categoria:</h5>
-                                <span>{post.category_post}</span>
-                            </div>
-
-                            <div>
-                                <h5>Tag:</h5>
-                                {post.tags_post.map((tag, index) => (
-                                    <span key={index}>#{tag} </span>
-                                ))}
-                            </div>
-                        </div>
+                        <label htmlFor="contentPost" className="form-label">
+                            <FontAwesomeIcon icon="fa-solid fa-box-open" />
+                            <strong> Contenuto:</strong>
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Inserisci qui il contenuto del post"
+                            name="content_post"
+                            value={formData.content_post}
+                            onChange={(e) => updateFormData(e.target.value, "content_post")}
+                        ></input>
                     </div>
 
-                ))}
+                    <div>
+                        <label htmlFor="categoryPost" className="form-label">
+                            <FontAwesomeIcon icon="fa-solid fa-certificate" />
+                            <strong> Categoria:</strong>
+                        </label><br />
+                        <select value={formData.category_post} onChange={(e) => updateFormData(e.target.value, 'category_post')}>
+                            <option value="">--Seleziona la categoria--</option>
+                            <option value="Nature Notes" /*selected={formData.category_post === 'nature_notes'}*/>Nature Notes</option>
+                            <option value="Travel Tidbits" /*selected={formData.category_post === 'travel_tidbits'}*/>Travel Tidbits</option>
+                            <option value="Nature's Palette" /*selected={formData.category_post === 'natures_palette'}*/>Nature's Palette</option>
+                            <option value="Wayfaring Waves" /*selected={formData.category_post === 'wayfaring_waves'}*/>Wayfaring Waves</option>
+                            <option value="Nomad Notes" /*selected={formData.category_post === 'nomad_notes'}*/>Nomad Notes</option>
+                            <option value="Sound Bites" /*selected={formData.category_post === 'sound_bites'}*/>Sound Bites</option>
+                        </select>
+                    </div>
+
+                    <label htmlFor="tagsPost" className="form-label py-2">
+                        <FontAwesomeIcon icon="fa-solid fa-hashtag" />
+                        <strong> Tags:</strong>
+                    </label>
+                    <div className='row'>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('LifeStyle')} value="LifeStyle" onChange={handleTagsPostChange} />#LifeStyle</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('MotivationMonday')} value="MotivationMonday" onChange={handleTagsPostChange} />#MotivationMonday</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('PicOfTheDay')} value="PicOfTheDay" onChange={handleTagsPostChange} />#PicOfTheDay</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('NaturePhotography')} value="NaturePhotography" onChange={handleTagsPostChange} />#NaturePhotography</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('AdventureTime')} value="AdventureTime" onChange={handleTagsPostChange} />#AdventureTime</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('MusicIsLife')} value="MusicIsLife" onChange={handleTagsPostChange} />#MusicIsLife</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('MusicInspiration')} value="MusicInspiration" onChange={handleTagsPostChange} />#MusicInspiration</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('VacationMode')} value="VacationMode" onChange={handleTagsPostChange} />#VacationMode</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('TravelPhotography')} value="TravelPhotography" onChange={handleTagsPostChange} />#TravelPhotography</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('DigitalWorld')} value="DigitalWorld" onChange={handleTagsPostChange} />#DigitalWorld</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('FilmFestival')} value="FilmFestival" onChange={handleTagsPostChange} />#FilmFestival</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('SocialMedia')} value="SocialMedia" onChange={handleTagsPostChange} />#SocialMedia</label>
+                        <label htmlFor="" className="col-4"><input type="checkbox" checked={formData.tags_post.includes('TagsForLike')} value="TagsForLike" onChange={handleTagsPostChange} />#TagsForLike</label>
+                    </div>
+
+                    <div>
+                        <button type="submit" className={`btn btn-primary my-4 ${style.btn_mod}`}>
+                            Crea Post
+                        </button>
+                    </div>
+
+                </form>
             </div>
+
+            <div className={style.posts_list_container}>
+                <div className={`row justify-content-center`}>
+                    {postsList.map((post) => (
+                        <div key={post.id} className={`col-5 mx-5 my-5 card ${style.mod_card}`}>
+
+                            <img className={style.card_img} src={post.img_post} alt="Post" />
+
+                            <div className={style.card_main}>
+                                <div className={style.card_header}>
+
+                                    <h3>{post.title_post}</h3>
+
+                                    <button className={style.delete_button} onClick={() => removePost(post.id)}>
+                                        <FontAwesomeIcon icon="fa-solid fa-trash" />
+                                    </button>
+
+                                </div>
+
+                                <p>{post.content_post}</p>
+
+                                <div>
+                                    <h5>Categoria:</h5>
+                                    <span>{post.category_post}</span>
+                                </div>
+
+                                <div>
+                                    <h5>Tag:</h5>
+                                    {post.tags_post.map((tag, index) => (
+                                        <span key={index}>#{tag} </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                    ))}
+                </div>
+            </div>
+
         </div>
     );
 }
